55030a62f194eafa29e4dcf2f0b8bc74b5949533
Kripa Prasad <prasad.kripa@gmail.com>
2024-09-22 11:57:06 +0530

    queryparams and params

--- src/app.js
+++ src/app.js
@@ -3,11 +3,33 @@ const app = express();
 
 
 
-app.use('/',(req,res)=>{
+// app.use('/',(req,res)=>{
 
-    res.send('Hello From dev Tinder');
+//     res.send('Hello From dev Tinder');
 
+// })
+
+
+app.get('/',(req,res)=>{
+   res.send("Hello from dev Tinder");
+    // res.json("hello from get")
 })
+
+//get the data from query params
+app.get('/hello',function(req,res){
+   console.log(req.query);
+    res.send('Msg from hello...')
+})
+
+//etting the id from url
+app.get('/test/:id',function(req,res){
+    console.log(req.params.id);
+     res.send('Msg from hello...')
+ })
+
+// app.get('/hello/:id',function(req,res){
+//     res.send('Msg from hello...')
+// })
 
 
 app.listen(1234,()=>{
